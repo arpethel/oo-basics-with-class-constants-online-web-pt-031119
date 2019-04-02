a27fea190d9b7a38af824eaea194f932a0de5a63
Ryan Pethel <arpethel@gmail.com>
2019-04-02 20:27:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  # attr_reader :brand
 
   BRANDS = []
 
@@ -12,6 +12,10 @@ class Shoe
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+
+  def brand
+    @brand
   end
 
   def brand=(brand)
