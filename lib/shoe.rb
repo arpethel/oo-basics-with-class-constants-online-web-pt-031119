edfbadeb4b692850e50204b3b252f454a95a8a79
Ryan Pethel <arpethel@gmail.com>
2019-04-02 20:52:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,14 @@
 require 'pry'
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition, :brand
   # attr_reader :brand
 
   BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    binding.pry
+    BRANDS << brand
   end
 
   def cobble
@@ -14,12 +16,12 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand
-    @brand
-  end
+  # def brand
+  #   @brand
+  # end
 
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+  # def brand=(brand)
+  #   @brand = brand
+  #   BRANDS << brand
+  # end
 end
