a77073be89fe483cdf6618b478ba507eb654af20
Ryan Pethel <arpethel@gmail.com>
2019-04-02 20:15:52 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,9 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_reader :brand, :brands
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
@@ -11,4 +14,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def brands=(brands)
+    @brands = brands
+    BRANDS << brands.size
+  end
 end
